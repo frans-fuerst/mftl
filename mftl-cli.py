f2f0b9a48b9b082a73fc66e997029fb4a79c32de
frans <frans.fuerst@gmail.com>
2017-06-28 07:38:00 +0200

    improve drawing trade plots, introduce triple ma in example trader

--- mftl-cli.py
+++ mftl-cli.py
@@ -5,6 +5,7 @@ import ast
 import logging as log
 import argparse
 import time
+import itertools
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import mftl
@@ -14,37 +15,55 @@ import mftl.qwtgraph
 FEE = 0.9975
 
 
+def sliced(iterable, N):
+    it = iter(iterable)
+    return list(iter(lambda: list(itertools.islice(it, N)), []))
+
+
 class Strategy:
-    def __init__(self, fast_ma, slow_ma):
+    def __init__(self, fast_ma, medium_ma, slow_ma):
         self._fast_ma = fast_ma
+        self._medium_ma = medium_ma
         self._slow_ma = slow_ma
         self._trades = []
         self._plots = []
 
     def feed(self, history, amount):
+        #    data = th.data()
+        #    trade_rates = [e['total'] / e['amount'] for e in data]
+        #    trade_times = [e['time'] - now for e in data]
+
+        # generate 5min-buckets
         bucket_data = history.rate_buckets()
         times = [e['time'] / 3600 for e in bucket_data]
         rates = [(e['total_sell'] + e['total_buy']) /
                  (e['amount_sell'] + e['amount_buy']) for e in bucket_data]
-        rates_slow = mftl.sma(rates, self._slow_ma)
         rates_fast = mftl.sma(rates, self._fast_ma)
-        times, rates, rates_fast, rates_slow = mftl.trim(times, rates, rates_fast, rates_slow)
+        rates_medium = mftl.sma(rates, self._medium_ma)
+        rates_slow = mftl.sma(rates, self._slow_ma)
+        times, rates, rates_fast, rates_medium, rates_slow = mftl.trim(
+            times, rates, rates_fast, rates_medium, rates_slow)
 
         self._plots.append((times, ((rates, 'black'),
-                                    (rates_slow, 'fat_blue'),
-                                    (rates_fast, 'fat_red'))))
+                                    (rates_medium, 'darkCyan_fat'),
+                                    (rates_fast, 'magenta_fat'),
+                                    (rates_slow, 'blue_fat'),
+                                    )))
 
         amount_C1 = amount  # amount of primary coin we start with
         amount_C2 = 0.      # amount of secondary coin (asset) we start with
 
-        trades = 0
         last_C1 = 0
         last_C2 = 0
         for i, d in enumerate(rates):
             if i == 0: continue
             # verkaufen, wenn fast MA
-            action = ('buy' if rates_fast[i] >= rates_slow[i] and rates_fast[i - 1] < rates_slow[i - 1] else
-                      'sell' if rates_fast[i] <= rates_slow[i] and rates_fast[i - 1] > rates_slow[i - 1] else
+            action = ('buy' if (rates_fast[i] >= rates_medium[i] and
+                                rates_fast[i - 1] < rates_medium[i - 1] and
+                                rates_fast[i] > rates_slow[i]) else
+                      'sell' if (rates_fast[i] <= rates_medium[i] and
+                                 rates_fast[i - 1] > rates_medium[i - 1] and
+                                 rates_fast[i] < rates_slow[i]) else
                       'none')
             if action == 'none': continue
             if action == 'buy':
@@ -58,21 +77,28 @@ class Strategy:
                 amount_C1 = new_c1
                 last_C2, amount_C2 = amount_C2, 0.
             self._trades.append((action, times[i], d))
-            trades += 1
-            print('%.4d %.7d %9.2f %11.2f %11.9f %s' % (
-                i, times[i], amount_C1, amount_C2, d, action))
+#            print('%.4d %.7d %9.2f %11.2f %11.9f %s' % (
+#                i, times[i], amount_C1, amount_C2, d, action))
 
+        if self._trades and self._trades[-1][0] == 'buy':
+            del self._trades[-1]
+
+        return last_C1
 
     def plot(self):
         w = mftl.qwtgraph.GraphUI()
-        #    w.set_data(trade_times, trade_rates, 'gray')
         for t, plots in self._plots:
             for curve, color in plots:
                 w.set_data(t, curve, color)
-        for a, t, v in self._trades:
-            w.add_vmarker(t, 'red' if a == 'sell' else 'green')
-            w.add_hmarker(v, 'red' if a == 'sell' else 'green')
+
+        for (a1, t1, v1), (a2, t2, v2) in sliced(self._trades, 2):
+            w.add_vmarker(t1, 'green')
+            w.add_vmarker(t2, 'red')
+            #            w.add_hmarker(v, 'red' if a == 'sell' else 'green')
+            w.add_line(t1, v1, t2, v2, 'green_fat' if v2 >= v1 else 'red_fat' )
+
         w.show()
+
 
 def show_curve(market):
     now = time.time()
@@ -83,15 +109,21 @@ def show_curve(market):
 
     print('%r, #trades: %d, duration: %.1fh' % (
         market, th.count(), th.duration() / 3600))
+    m = (0, market, 0, 0, 0)
 
-#    data = th.data()
+    #for f in range(20):
+        #for s in range(20):
+            #fast = f + 1
+            #slow = fast + s + 1
 
-#    trade_rates = [e['total'] / e['amount'] for e in data]
-#    trade_times = [e['time'] - now for e in data]
+    fast = 20
+    medium = 35
+    slow = 70
 
-    # generate 5min-buckets
-    strategy = Strategy(10, 50)
-    strategy.feed(th, 100)
+    strategy = Strategy(fast, medium, slow)
+    g = strategy.feed(th, 100), market, fast, medium, slow
+    m = max(g, m)
+    print(g, m)
     strategy.plot()
 
 
